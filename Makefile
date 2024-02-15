136fc718af657c8b1b3512f01410697eb79971e6
Rafal Janicki <bloorq@gmail.com>
2024-02-15 21:19:19 +0000

    Basic template code

---
+++ Makefile
@@ -0,0 +1,8 @@
+## Installation
+install:
+	python3 -m pip install -r requirements.txt
+
+## Run 2098
+run:
+	python3 ./main.py
+
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+import os
+
+import src.utils.keyboard as keyboard
+from src.game.game import Game
 
 if __name__ == "__main__":
-    print("2098")
+    game = Game()
+    try:
+        while True:
+            key_pressed = keyboard.getkey()
+            if key_pressed == 'esc' or key_pressed == 'q':
+                game.save()
+                quit()
+            else:
+                # main loop
+                game.play(key_pressed)
+
+    except (KeyboardInterrupt, SystemExit):
+        os.system('stty sane')
+        print('stopping.')
